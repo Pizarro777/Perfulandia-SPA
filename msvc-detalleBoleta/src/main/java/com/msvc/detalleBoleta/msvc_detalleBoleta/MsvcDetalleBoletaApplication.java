4440b6be7d341da7fbc5edc1bd707174f28cf7e4
Alexander1-svg <al.bellod@duocuc.cl>
2025-05-17 18:50:23 -0400

    add Detalle de boleta

---
+++ msvc-detalleBoleta/src/main/java/com/msvc/detalleBoleta/msvc_detalleBoleta/MsvcDetalleBoletaApplication.java
@@ -0,0 +1,13 @@
+package com.msvc.detalleBoleta.msvc_detalleBoleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcDetalleBoletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcDetalleBoletaApplication.class, args);
+	}
+
+}
