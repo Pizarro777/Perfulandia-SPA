e02045c5176d89fce43ff455303c8bb42990403e
Juan <ju.pizarrol@duocuc.cl>
2025-05-17 00:31:11 -0400

    Agregando Proyecto

---
+++ msvc-productos/src/test/java/com/pizarro777/msvc/productos/MsvcProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizarro777.msvc.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
