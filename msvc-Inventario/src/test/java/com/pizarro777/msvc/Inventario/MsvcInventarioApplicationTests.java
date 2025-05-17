eca7eaa080447a17e5bfcec897c61b7209be8129
Pizarro777 <ju.pizarrol@duocuc.cl>
2025-05-17 18:26:50 -0400

    Agregando microservicio de inventario

---
+++ msvc-Inventario/src/test/java/com/pizarro777/msvc/Inventario/MsvcInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizarro777.msvc.Inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ msvc-productos/src/main/java/com/pizarro777/msvc/productos/controllers/ProductoController.java
@@ -0,0 +1,15 @@
+package com.pizarro777.msvc.productos.controllers;
+
+import com.pizarro777.msvc.productos.repositories.ProductoRepository;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/productos")
+@Validated
+public class ProductoController {
+    private final ProductoService service;
+
+
+}
