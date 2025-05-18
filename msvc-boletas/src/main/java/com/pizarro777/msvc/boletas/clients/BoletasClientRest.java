2f661af53f7be763ad60f7c008ac0ca8bf821585
Abraham2143 <ab.neira@duocuc.cl>
2025-05-17 21:24:49 -0400

    guardando

---
+++ msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/clients/BoletasClientRest.java
@@ -0,0 +1,14 @@
+package com.pizarro777.msvc.boletas.clients;
+
+import com.pizarro777.msvc.boletas.models.Boletas;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name= "msvc-boletas", url = "localhost:8080/api/v1/boletas")
+public interface BoletasClientRest {
+
+    @GetMapping("/{id}")
+    Boletas findById@PathVariable Long id);
+
+}
