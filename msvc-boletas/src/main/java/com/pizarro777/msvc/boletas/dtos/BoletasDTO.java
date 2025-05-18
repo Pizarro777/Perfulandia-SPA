bdeb0798e53018f1af8928b7a931251fe8d19769
Abraham2143 <ab.neira@duocuc.cl>
2025-05-17 20:40:30 -0400

    guardando

--- msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/dtos/BoletasDTO.java
+++ msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/dtos/BoletasDTO.java
@@ -1,4 +1,15 @@
 package com.pizarro777.msvc.boletas.dtos;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoletasDTO {
+
+    private String numeroBoleta;
+    private int cantidadBoletas;
+    private double precioBoletas;
 }
--- msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/models/BoletasModel.java
+++ msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/models/BoletasModel.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Getter @Setter @ToString
 @AllArgsConstructor @NoArgsConstructor
 public class BoletasModel {
-
+    private Long idBoletas;
     private String numeroBoleta;
     private int cantidadBoletas;
     private double precioBoletas;
