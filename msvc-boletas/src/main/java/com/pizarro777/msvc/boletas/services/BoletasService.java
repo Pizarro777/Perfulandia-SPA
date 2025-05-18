3f6a6058e26c1bee9a4fcfeeace74abbba09d870
Abraham2143 <ab.neira@duocuc.cl>
2025-05-17 21:14:28 -0400

    guardando

--- msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/services/BoletasService.java
+++ msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/services/BoletasService.java
@@ -1,4 +1,11 @@
 package com.pizarro777.msvc.boletas.services;
 
-public class BoletasService {
+import com.pizarro777.msvc.boletas.models.Boletas;
+
+import java.util.List;
+
+public interface BoletasService {
+    List<Boletas> findAll();
+    Boletas findById(int id);
+    Boletas save(Boletas boletas);
 }
