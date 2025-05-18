d3709b9fd48dc7ca6d782b2aeee8b29d54e0b614
Abraham2143 <ab.neira@duocuc.cl>
2025-05-17 21:20:25 -0400

    guardando

--- msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/services/BoletasServicelmpl.java
+++ msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/services/BoletasServicelmpl.java
@@ -1,4 +1,10 @@
 package com.pizarro777.msvc.boletas.services;
 
+import com.pizarro777.msvc.boletas.repositories.BoletasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class BoletasServicelmpl {
+
+    @Autowired
+    private BoletasRepository boletasRepository;
 }
