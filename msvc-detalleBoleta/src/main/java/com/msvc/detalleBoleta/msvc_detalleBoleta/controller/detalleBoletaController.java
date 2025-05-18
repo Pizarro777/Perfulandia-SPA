e02286e44f3871964a280a142aef3ac2e54bdb69
Alexander1-svg <al.bellod@duocuc.cl>
2025-05-17 20:07:38 -0400

    Creando Models de DetalleBoleta

--- msvc-detalleBoleta/src/main/java/com/msvc/detalleBoleta/msvc_detalleBoleta/controller/detalleBoletaController.java
+++ msvc-detalleBoleta/src/main/java/com/msvc/detalleBoleta/msvc_detalleBoleta/controller/detalleBoletaController.java
@@ -1,8 +1,14 @@
 package com.msvc.detalleBoleta.msvc_detalleBoleta.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/detalleBoleta")
+@Validated
 public class detalleBoletaController {
+
 
 }
--- msvc-detalleBoleta/src/main/java/com/msvc/detalleBoleta/msvc_detalleBoleta/model/detalleBoletaModel.java
+++ msvc-detalleBoleta/src/main/java/com/msvc/detalleBoleta/msvc_detalleBoleta/model/detalleBoletaModel.java
@@ -1,4 +1,31 @@
 package com.msvc.detalleBoleta.msvc_detalleBoleta.model;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.aspectj.bridge.IMessage;
+
+@Entity
+@Table(name="DetalleBoletas")
+@Getter @Setter @AllArgsConstructor
+@NoArgsConstructor @ToString
 public class detalleBoletaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idDetalleBoleta")
+    private Long idDetalleBoleta;
+
+    @Column(name = "precioUnitario", nullable = false)
+    @NotNull(message = "El campo precio no puede ser vacio")
+    private Double precioUnitario;
+
+    @Column(name = "cantidad", nullable = false)
+    @NotNull(message = "El campo cantidad no puede ser vacio")
+    private Integer cantidad;
+
+    @Column(name = "idProducto", nullable = false)
+    @NotNull(message = "El campo idProducto no puede ser vacio")
+
 }
