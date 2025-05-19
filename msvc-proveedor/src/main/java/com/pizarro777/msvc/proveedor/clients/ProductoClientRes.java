5a60c55a3c80a8cffc672bf8b8a890b5473752e9
Abraham <ab.neira@duocuc.cl>
2025-05-19 11:40:19 -0400

    guardando

---
+++ msvc-proveedor/src/main/java/com/pizarro777/msvc/proveedor/clients/ProductoClientRes.java
@@ -0,0 +1,14 @@
+package com.pizarro777.msvc.proveedor.clients;
+
+import com.pizarro777.msvc.productos.dtos.ProductoDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "msvc-productos", url = "localhost:8080")
+public interface ProductoClientRes {
+
+    @GetMapping("/productos/{idProducto}")
+    ProductoDTO obtnerProductoPorId(@PathVariable("idProducto") Long idProducto);
+
+}
---
+++ msvc-proveedor/src/main/java/com/pizarro777/msvc/proveedor/models/Proveedor.java
@@ -0,0 +1,8 @@
+package com.pizarro777.msvc.proveedor.models;
+
+public class Proveedor {
+    private String nombre;
+    private String direccion;
+    private Integer telefono;
+    private String servicio;
+}
