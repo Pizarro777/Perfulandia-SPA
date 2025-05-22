0b210c6559c1b50b9287fffd10b72e5c4a6bf96a
Abraham2143 <ab.neira@duocuc.cl>
2025-05-22 09:55:48 -0400

    guardando

--- msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/repositories/BoletasRepository.java
+++ msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/repositories/BoletasRepository.java
@@ -2,8 +2,16 @@ package com.pizarro777.msvc.boletas.repositories;
 
 import com.pizarro777.msvc.boletas.models.Boletas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoletasRepository extends JpaRepository<Boletas, Long> {
+
+    @Query("SELECT b FROM Boletas b WHERE b.nombre = :nombre")
+    List<Boletas> buscarPorNombre(@Param("nombres") String nombre);
+
 }
