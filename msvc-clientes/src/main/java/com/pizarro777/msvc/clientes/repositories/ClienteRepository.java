f8f4c46bae52e30f494326fe2709a66d66933879
Pizarro777 <ju.pizarrol@duocuc.cl>
2025-05-22 10:30:15 -0400

    Actualizando Cliente Repository

--- msvc-clientes/src/main/java/com/pizarro777/msvc/clientes/repositories/ClienteRepository.java
+++ msvc-clientes/src/main/java/com/pizarro777/msvc/clientes/repositories/ClienteRepository.java
@@ -1,30 +1,29 @@
-package com.pizarro777.msvc.clientes.repositories;
+    package com.pizarro777.msvc.clientes.repositories;
 
-import com.pizarro777.msvc.clientes.models.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import java.util.List;
+    import com.pizarro777.msvc.clientes.models.Cliente;
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.data.jpa.repository.Query;
+    import org.springframework.data.repository.query.Param;
+    import org.springframework.stereotype.Repository;
+    import java.util.List;
 
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    @Repository
+    public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
-    /* Encuentra clientes por apellidos */
-    @Query("SELECT c FROM cliente c WHERE c.apellidos= : apellido")
-    List<Cliente> findbyApellido(@Param("apellidos") String apellido);
-
-
-    /* Encuentra clientes por correo */
-    @Query("SELECT c FROM cliente c WHERE c.correos= :correo")
-    Cliente findByCorreo(String correo);
+        /* Encuentra clientes por apellidos */
+        @Query("SELECT c FROM Cliente c WHERE c.apellidos= : apellido")
+        List<Cliente> findbyApellido(@Param("apellidos") String apellido);
 
 
-    /* Encuentra clientes por nombre y apellidos */
-    @Query("SELECT c FROM cliente c WHERE c.nombres= : nombre")
-    @Query("SELECT c FROM cliente c WHERE c.apellidos= : apellido")
-    List<Cliente> findByNombreandApellido(String nombre, String apellido);
+        /* Encuentra clientes por correo */
+        @Query("SELECT c FROM Cliente c WHERE c.correos= :correo")
+        Cliente findByCorreo(@Param("correo") String correo);
+
+
+        /* Encuentra clientes por nombre y apellidos */
+        @Query("SELECT c FROM Cliente c WHERE c.nombres = :nombre AND c.apellidos = :apellido")
+        List<Cliente> findByNombreandApellido(@Param("nombre") String nombre, @Param("apellido") String apellido);
 
 
 
-}
+    }
