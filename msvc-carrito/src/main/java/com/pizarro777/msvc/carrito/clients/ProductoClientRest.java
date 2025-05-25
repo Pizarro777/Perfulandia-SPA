99ff129f2f73414036b271b491755ebb41ec1a48
Alexander1-svg <al.bellod@duocuc.cl>
2025-05-25 19:25:19 -0400

    FIX: Client(MSVC-CARRITO)

--- msvc-carrito/src/main/java/com/pizarro777/msvc/carrito/clients/ProductoClientRest.java
+++ msvc-carrito/src/main/java/com/pizarro777/msvc/carrito/clients/ProductoClientRest.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-producto", url = "localhost:8007")
-public class ProductoClientRest {
+@FeignClient(name = "msvc-productos", url = "localhost:8002")
+public interface ProductoClientRest {
 
     @GetMapping("/productos/{id}")
-    String obtenerProductoPorId(@PathVariable("id")Long id);
+    String obtenerProductoPorId(@PathVariable("id") Long id);
 }
