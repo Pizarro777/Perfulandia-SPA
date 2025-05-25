0b61880ce42a557b264d30031039a75dc624908b
Abraham2143 <ab.neira@duocuc.cl>
2025-05-25 08:48:10 -0400

    guardando

--- msvc-proveedor/src/main/java/com/pizarro777/msvc/proveedor/clients/BoletaClientRest.java
+++ msvc-proveedor/src/main/java/com/pizarro777/msvc/proveedor/clients/BoletaClientRest.java
@@ -1,6 +1,5 @@
 package com.pizarro777.msvc.proveedor.clients;
 
-import com.pizarro777.msvc.boletas.dtos.BoletasDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "msvc-boletas", url = "localhost:8080")
 public interface BoletaClientRest {
 
-    @GetMapping("/boletas/{idBoletas}")
-    BoletasDTO obtenerBoletaPorId(@PathVariable("idBoletas") Long idBoletas);
+    @GetMapping("/boletas/{id}")
+    String obtenerBoletaPorId(@PathVariable("id") Long id);
 
 }
