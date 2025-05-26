2e43040e368a6ef08af9ed174fa7a3cc6bb78e24
Pizarro777 <ju.pizarrol@duocuc.cl>
2025-05-26 01:21:20 -0400

    Fix: Scripting

--- msvc-productos/src/main/java/com/pizarro777/msvc/productos/repositories/ProductoRepository.java
+++ msvc-productos/src/main/java/com/pizarro777/msvc/productos/repositories/ProductoRepository.java
@@ -3,7 +3,6 @@ package com.pizarro777.msvc.productos.repositories;
 import com.pizarro777.msvc.productos.models.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
