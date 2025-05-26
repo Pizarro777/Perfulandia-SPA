7523993fbe18ac37de580a9f5a125594394d5b40
Abraham2143 <ab.neira@duocuc.cl>
2025-05-26 11:37:58 -0400

    guardando

--- msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/clients/CarritoClientRest.java
+++ msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/clients/CarritoClientRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-carrito", url = "http://localhost:8002/api/carrito")
+@FeignClient(name = "msvc-carrito", url = "http://localhost:8001/api/carrito")
 public interface CarritoClientRest {
 
     @GetMapping("/{id}")
--- msvc-proveedor/src/main/java/com/pizarro777/msvc/proveedor/clients/BoletaClientRest.java
+++ msvc-proveedor/src/main/java/com/pizarro777/msvc/proveedor/clients/BoletaClientRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-boletas", url = "localhost:8080")
+@FeignClient(name = "msvc-boletas", url = "localhost:8000/api/boletas")
 public interface BoletaClientRest {
 
     @GetMapping("/boletas/{id}")
