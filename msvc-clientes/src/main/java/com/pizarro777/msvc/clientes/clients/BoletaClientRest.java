14de8f963f4e9038c58ec4bc5d906c81cacbd2c3
Pizarro777 <ju.pizarrol@duocuc.cl>
2025-05-26 12:27:42 -0400

    Fix: bug en carrito y comentarios

---
+++ msvc-clientes/src/main/java/com/pizarro777/msvc/clientes/clients/BoletaClientRest.java
@@ -0,0 +1,13 @@
+package com.pizarro777.msvc.clientes.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "msvc-boletas", url= "http://localhost:8000/api/v1/boletas")
+public interface BoletaClientRest {
+
+    @GetMapping("/{id}")
+    String obtenerBoleta(@PathVariable Long idComentario);
+
+}
