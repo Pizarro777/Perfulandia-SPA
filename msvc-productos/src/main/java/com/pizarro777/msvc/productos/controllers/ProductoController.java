f92da2353095b4bbe463a215bf5211465deb122b
Pizarro777 <ju.pizarrol@duocuc.cl>
2025-06-10 11:14:35 -0400

    Update: Implementacion de Swagger

--- msvc-productos/src/main/java/com/pizarro777/msvc/productos/controllers/ProductoController.java
+++ msvc-productos/src/main/java/com/pizarro777/msvc/productos/controllers/ProductoController.java
@@ -3,6 +3,12 @@ package com.pizarro777.msvc.productos.controllers;
 import com.pizarro777.msvc.productos.models.Producto;
 import com.pizarro777.msvc.productos.repositories.ProductoRepository;
 import com.pizarro777.msvc.productos.services.ProductoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/productos")
 @Validated
+@Tag(name="Productos", description = "Esta sección contiene los CRUD de Productos")
 public class ProductoController {
 
     @Autowired
@@ -21,12 +28,13 @@ public class ProductoController {
     @Autowired
     private ProductoRepository productoRepository;
 
+
     public ProductoController(ProductoService service){
         this.service = service;
     }
 
     /* Crear un nuevo Producto */
-    @PostMapping("/make")
+    @PostMapping()
     public ResponseEntity<Producto> crearProducto(@RequestBody @Valid Producto producto){
         Producto prod = service.crearProducto(producto);
         return ResponseEntity.status(201).body(prod);
@@ -40,7 +48,19 @@ public class ProductoController {
     }
 
     /* Obtener todos los productos */
-    @GetMapping("/todos")
+    @GetMapping()
+    @Operation(
+            summary = "Devuelve todos los productos",
+            description = "Este metodo debe retornar un List de Producto, en caso "+
+                    "de que no encuentre nada retorna "
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se retornaron todos los clientes ok"),
+            @ApiResponse(responseCode = "400", description = "Error - Producto con ID no existente")
+    })
+    @Parameters(value ={
+            @Parameter(name= "id", description= "")
+    })
     public List<Producto> obtenerTodos(){
         return service.listarTodos();
     }
