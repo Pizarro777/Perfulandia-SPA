6b4b382d0c35020409ae31c83a27f85adc8943ff
Pizarro777 <ju.pizarrol@duocuc.cl>
2025-06-24 01:15:35 -0400

    fix: Bug de ProductoController

--- msvc-productos/src/main/java/com/pizarro777/msvc/productos/controllers/ProductoController.java
+++ msvc-productos/src/main/java/com/pizarro777/msvc/productos/controllers/ProductoController.java
@@ -36,6 +36,13 @@ public class ProductoController {
 
     /* Crear un nuevo Producto */
     @PostMapping()
+    @Operation(
+            summary = "Crea un nuevo Producto.",
+            description = "Este metodo debe crear un producto nuevo.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "El producto ha sido creado."),
+            @ApiResponse(responseCode = "400", description = "Error - producto no se logro crear.")
+    })
     public ResponseEntity<ProductoOutputDTO> crearProducto(@RequestBody @Validated ProductoInputDTO producto){
         Producto prod = service.crearProducto(producto);
         return ResponseEntity.status(201).body(service.EntityToDTO(prod));
@@ -43,6 +50,15 @@ public class ProductoController {
 
     /* Obtener un producto por ID */
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Devuelve un Producto por id.",
+            description = "Este metodo debe retornar un Producto, en caso "+
+                    "de que no encuentre nada retorna."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se retorno el producto ok."),
+            @ApiResponse(responseCode = "400", description = "Error - Producto con ID no existente.")
+    })
     public ResponseEntity<ProductoOutputDTO> obtenerProducto(@PathVariable("id") Long id){
         return ResponseEntity.status(201).body(service.obtenerPorId(id));
     }
@@ -50,7 +66,7 @@ public class ProductoController {
     /* Obtener todos los productos */
     @GetMapping()
     @Operation(
-            summary = "Devuelve todos los productos",
+            summary = "Devuelve todos los Producto",
             description = "Este metodo debe retornar un List de Producto, en caso "+
                     "de que no encuentre nada retorna "
     )
@@ -64,12 +80,31 @@ public class ProductoController {
 
     /* Actualizar Producto */
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Actualiza un Producto por id.",
+            description = "Este metodo debe actualizar un Producto por id, en caso "+
+                    "de que no encuentre nada retorna."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se Actualizo el producto ok."),
+            @ApiResponse(responseCode = "400", description = "Error - Producto con ID no existente.")
+    })
     public ResponseEntity<ProductoOutputDTO> actualizarProducto(@PathVariable Long id, @RequestBody @Validated ProductoInputDTO producto){
         return ResponseEntity.ok(service.actualizarProducto(id, producto));
     }
 
+
     /* Eliminar un producto por ID */
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Elimina un Producto por id.",
+            description = "Este metodo debe eliminar un Producto, en caso "+
+                    "de que no encuentre no eliminara nada."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se elimino el producto ok."),
+            @ApiResponse(responseCode = "400", description = "Error - Producto con ID no existente.")
+    })
     public ResponseEntity<Void> eliminarProducto(@PathVariable Long id){
         service.eliminarProducto(id);
         return ResponseEntity.noContent().build();
