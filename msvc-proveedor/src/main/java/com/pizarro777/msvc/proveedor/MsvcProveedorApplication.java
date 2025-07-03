e33c0230bf9aa606806d03c002a143c4a5d0d7ec
Abraham2143 <ab.neira@duocuc.cl>
2025-07-03 18:19:08 -0400

    msvc, pom,  de proveedor

--- msvc-proveedor/src/main/java/com/pizarro777/msvc/proveedor/MsvcProveedorApplication.java
+++ msvc-proveedor/src/main/java/com/pizarro777/msvc/proveedor/MsvcProveedorApplication.java
@@ -2,7 +2,9 @@ package com.pizarro777.msvc.proveedor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 @SpringBootApplication
 public class MsvcProveedorApplication {
 
