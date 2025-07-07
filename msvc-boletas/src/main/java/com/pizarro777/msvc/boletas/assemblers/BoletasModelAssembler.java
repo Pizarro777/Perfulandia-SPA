2fe9a491dddf6f925cae43764feb031c8f493ab1
Abraham2143 <ab.neira@duocuc.cl>
2025-07-07 12:45:07 -0400

    model assembler de boleta y proveedor

--- msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/assemblers/BoletasModelAssembler.java
+++ msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/assemblers/BoletasModelAssembler.java
@@ -3,6 +3,7 @@ package com.pizarro777.msvc.boletas.assemblers;
 import com.pizarro777.msvc.boletas.controllers.BoletasControllerV2;
 import com.pizarro777.msvc.boletas.models.entities.Boletas;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ public class BoletasModelAssembler implements RepresentationModelAssembler<Bolet
 
     @Override
     public EntityModel<Boletas> toModel(Boletas entity) {
+        Link link = Link.of("http://localhost:8000/api/boletas/" + entity.getIdBoletas()).withRel("boletas");
         return EntityModel.of(
                 entity,
                 linkTo(methodOn(BoletasControllerV2.class).findBoletasProductoDTOById(entity.getIdBoletas())).withSelfRel(),
--- msvc-proveedor/src/main/java/com/pizarro777/msvc/proveedor/assemblers/ProveedorModelAssembler.java
+++ msvc-proveedor/src/main/java/com/pizarro777/msvc/proveedor/assemblers/ProveedorModelAssembler.java
@@ -1,8 +1,9 @@
 package com.pizarro777.msvc.proveedor.assemblers;
 
-import com.pizarro777.msvc.proveedor.dtos.ProveedorControllerV2;
+import com.pizarro777.msvc.proveedor.controllers.ProveedorControllerV2;
 import com.pizarro777.msvc.proveedor.models.entities.Proveedor;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,7 @@ public class ProveedorModelAssembler implements RepresentationModelAssembler<Pro
 
     @Override
     public EntityModel<Proveedor> toModel(Proveedor entity) {
+        Link link = Link.of("http://localhost:8000/api/proveedor/" + entity.getIdProveedor()).withRel("proveedor");
         return EntityModel.of(
                 entity,
                 linkTo(methodOn(ProveedorControllerV2.class).findById(entity.getIdProveedor())).withSelfRel(),
