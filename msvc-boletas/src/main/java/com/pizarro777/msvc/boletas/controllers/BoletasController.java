34f321a121edc706388ace3a311b74d39f3be468
Abraham2143 <ab.neira@duocuc.cl>
2025-07-09 19:23:37 -0400

    controller boletas

--- msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/controllers/BoletasController.java
+++ msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/controllers/BoletasController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -29,6 +30,7 @@ public class BoletasController {
 
     private final BoletasService boletasService;
 
+    @Autowired
     public BoletasController(BoletasService boletasService) {
         this.boletasService = boletasService;
     }
