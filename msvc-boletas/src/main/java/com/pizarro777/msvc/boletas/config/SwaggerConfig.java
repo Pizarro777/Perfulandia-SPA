b04b289e82e5f0e5bbd6a06a1f813df4e0f22674
Abraham2143 <ab.neira@duocuc.cl>
2025-07-09 21:00:33 -0400

    swagger de boletas

--- msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/config/SwaggerConfig.java
+++ msvc-boletas/src/main/java/com/pizarro777/msvc/boletas/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.pizarro777.msvc.boletas.config;
 
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,16 +12,12 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI(){
-        Contact contact = new Contact();
-        contact.setName("Abraham Neira");
-        contact.setEmail("abrahamneira1@gmail.com");
         return new OpenAPI()
                 .info(new Info()
                         .title("API - MSVC - Boletas")
                         .version("1.0.0")
                         .description("Este es el microservicio de Boletas, con el puedes realizar todas las consultas" +
                                 " CRUD que necesites")
-                        .contact(contact)
                         .summary("Esto es una api dentro de un proyecto de MSVC")
         );
     }
